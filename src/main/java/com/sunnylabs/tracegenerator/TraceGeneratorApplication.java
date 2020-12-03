db47b99064d45ebff691af43d8ce96ef9f915379
Peter Stone <stonepe@vmware.com>
2020-12-03 12:05:52 -0700

    Initialize Spring Boot

---
+++ src/main/java/com/sunnylabs/tracegenerator/TraceGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.sunnylabs.tracegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunnylabs/tracegenerator/TraceGeneratorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sunnylabs.tracegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = "management.metrics.export.wavefront.enabled=false")
+@SpringBootTest
+class TraceGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
